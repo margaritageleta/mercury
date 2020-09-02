639a765c8b5d6989e8fd1942835dd34bdeb11fd5
margaritageleta <noticiasmundiales99@gmail.com>
2020-09-02 16:51:46 +0200

    Hover things and minimize row widgets

--- static/js/rowWidget.js
+++ static/js/rowWidget.js
@@ -18,15 +18,37 @@ function addRowWidget(globals) {
                     <div>
                         <h1>Row widget `+globals.rowWidgetsCounter+`</h1>
                     </div>
-                    <input id='btn-remove-row-`+globals.rowWidgetsCounter+`' type='button' value='Remove widget'/>
+                    <div style="display: flex; width: max-content; flex-direction: row;">
+                        <div id='btn-minimize-row-`+globals.rowWidgetsCounter+`' class="controller-menu-button"><i class="fa fa-minus minus"></i></div>
+                        <div id='btn-remove-row-`+globals.rowWidgetsCounter+`' class="controller-menu-button"><i class="fa fa-times times"></i></div>
+                    </div>
                 </div>
-                <div id="row-widget-controller-options-` + globals.rowWidgetsCounter+`" class="controller-options">
-                    <input id="slider-margin-top-row-`+globals.rowWidgetsCounter+`" type="range" name="margin-row-`+globals.rowWidgetsCounter+`" min="0" max="50" value="0" step="1"/>
-                    <input id="slider-margin-bottom-row-`+globals.rowWidgetsCounter+`" type="range" name="margin-row-`+globals.rowWidgetsCounter+`" min="0" max="50" value="0" step="1"/>
-                    <input id="slider-margin-left-row-`+globals.rowWidgetsCounter+`" type="range" name="margin-row-`+globals.rowWidgetsCounter+`" min="0" max="50" value="0" step="1"/>
-                    <input id="slider-margin-right-row-`+globals.rowWidgetsCounter+`" type="range" name="margin-row-`+globals.rowWidgetsCounter+`" min="0" max="50" value="0" step="1"/>
+                <div class="row-controller-options subwidget-visible-`+globals.rowWidgetsCounter+`">
+                    <div>
+                        <div>
+                            <label>Top row margin</label>
+                            <input id="slider-margin-top-row-`+globals.rowWidgetsCounter+`" type="range" name="margin-row-`+globals.rowWidgetsCounter+`" min="0" max="50" value="0" step="1"/>
+                        </div>
+                        <div>
+                            <label>Bottom row margin</label>
+                            <input id="slider-margin-bottom-row-`+globals.rowWidgetsCounter+`" type="range" name="margin-row-`+globals.rowWidgetsCounter+`" min="0" max="50" value="0" step="1"/>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <label>Left row margin</label>
+                            <input id="slider-margin-left-row-`+globals.rowWidgetsCounter+`" type="range" name="margin-row-`+globals.rowWidgetsCounter+`" min="0" max="50" value="0" step="1"/>
+                        </div>
+                        <div>
+                            <label>Right row margin</label>
+                            <input id="slider-margin-right-row-`+globals.rowWidgetsCounter+`" type="range" name="margin-row-`+globals.rowWidgetsCounter+`" min="0" max="50" value="0" step="1"/>
+                        </div>
+                    </div>
                 </div>
-                <div style="width: 100%; display: flex; justify-content: center;">
+                <div id="row-widget-controller-options-` + globals.rowWidgetsCounter+`" class="controller-options subwidget-visible-`+globals.rowWidgetsCounter+`">
+                    
+                </div>
+                <div style="width: 100%; display: flex; justify-content: center;" class="subwidget-visible-`+globals.rowWidgetsCounter+`">
                         <input id="btn-add-text-widget-row-`+globals.rowWidgetsCounter+`" type="button" value="Add text element"/>
                         <input id="btn-add-splitter-row-`+globals.rowWidgetsCounter+`" type="button" value="Add splitter"/>
                         <input id="btn-add-image-widget-row-`+(globals.rowWidgetsCounter++)+`" type="button" value="Add image"/>
