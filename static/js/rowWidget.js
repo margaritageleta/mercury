475bc0c37007c8c4c3e8c9108d5f6f99ae838028
margaritageleta <noticiasmundiales99@gmail.com>
2020-09-02 13:26:07 +0200

    Add sliders to row widgets

--- static/js/rowWidget.js
+++ static/js/rowWidget.js
@@ -3,9 +3,11 @@ function addRowWidget(globals) {
     const widget = document.createElement('div');
     widget.setAttribute("id", "row-widget-" + globals.rowWidgetsCounter);
     console.log(globals.rowWidgetsCounter);
-    
+
     widget.className = 'row-widget';
-    widget.innerHTML = ``;
+    widget.innerHTML = `<div id="row-widget-content-`+globals.rowWidgetsCounter+`">
+        <p>Baby</p>
+    </div>`;
     document.getElementById('content__page_1').appendChild(widget);
 
     const widgetController = document.createElement('div');
@@ -16,17 +18,15 @@ function addRowWidget(globals) {
             <div>
                 <div class='controller-menu'>
                     <div>
-                        <h1>Row widget #`+globals.rowWidgetsCounter+`</h1>
+                        <h1>Row widget `+globals.rowWidgetsCounter+`</h1>
                     </div>
                     <input id='btn-remove-row-`+globals.rowWidgetsCounter+`' type='button' value='Remove widget'/>
                 </div>
                 <div id="row-widget-controller-options-` + globals.rowWidgetsCounter+`" class="controller-options">
-                    <!--<textarea 
-                        id="text-widget-`+(globals.textWidgetsCounter)+`" 
-                        name="text" 
-                        placeholder="Type here widget text"
-                        rows="4"
-                    ></textarea>-->
+                    <input id="slider-margin-top-row-`+globals.rowWidgetsCounter+`" type="range" name="margin-row-`+globals.rowWidgetsCounter+`" min="0" max="50" value="0" step="1"/>
+                    <input id="slider-margin-bottom-row-`+globals.rowWidgetsCounter+`" type="range" name="margin-row-`+globals.rowWidgetsCounter+`" min="0" max="50" value="0" step="1"/>
+                    <input id="slider-margin-left-row-`+globals.rowWidgetsCounter+`" type="range" name="margin-row-`+globals.rowWidgetsCounter+`" min="0" max="50" value="0" step="1"/>
+                    <input id="slider-margin-right-row-`+globals.rowWidgetsCounter+`" type="range" name="margin-row-`+globals.rowWidgetsCounter+`" min="0" max="50" value="0" step="1"/>
                 </div>
                 <div style="width: 100%; display: flex; justify-content: center;">
                         <input id="btn-add-text-widget-row-`+globals.rowWidgetsCounter+`" type="button" value="Add text element"/>
